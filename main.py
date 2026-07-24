26f3a028344a8ad979efee468ef2db5a1f12aa1e
micdev-07 <mumberemichael98@gmail.com>
2026-07-24 20:08:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,8 @@ app = FastAPI(title="NEMO Engine - Full Featured")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=["*"], # Je mettrai ici le lien vers le frontend en ligne
+    allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
 )
